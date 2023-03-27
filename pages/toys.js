9f681efb38a9a33e6ac2c48833efd5c8050f845f
Thomas Percival <thomaspercival@live.co.uk>
2023-03-27 17:11:41 +0100

    Create toys.js

---
+++ pages/toys.js
@@ -0,0 +1,12 @@
+import Navbar from "../components/navbar";
+
+const Toys = () => {
+  return (
+    <div>
+      <Navbar />
+      <div>Toys catalogue</div>
+    </div>
+  );
+};
+
+export default Toys;
