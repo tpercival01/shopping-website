885affbc0e8299bf99cf8072e5e057bd8d89be1d
Thomas Percival <thomaspercival@live.co.uk>
2023-03-27 17:11:12 +0100

    Create acc.js

---
+++ pages/acc.js
@@ -0,0 +1,12 @@
+import Navbar from "../components/navbar";
+
+const Acc = () => {
+  return (
+    <div>
+      <Navbar />
+      <div>Accessory catalogue</div>
+    </div>
+  );
+};
+
+export default Acc;
